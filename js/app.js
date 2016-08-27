336ad992969970984abad3120ed5bbd8d29929b4
Phoebe Lau <phoebejlau.la@gmail.com>
2016-08-27 12:45:11 -0700

    fizzbuzz completed

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  for (var number = 1; number <= 100; number++) {
+    if (number%3==0 && number%5==0) {
+      console.log("fizzbuzz");
+    }
+    else if (number%3==0) {
+      console.log("fizz");
+    }
+    else if (number%5==0) {
+      console.log("buzz");
+    }
+    else {
+      console.log(number);
+    }
+  }
+});
+
